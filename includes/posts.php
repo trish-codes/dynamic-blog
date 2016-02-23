4b169e58a586483237f5653c95bd9aea2862ad11
trish <trish.codes@gmail.com>
2016-02-23 12:56:59 -0600

    dynamic sidebar/posts

---
+++ includes/posts.php
@@ -0,0 +1,18 @@
+<?php
+
+    // create array of blog posts
+
+    $posts = array(
+                
+                array(
+                    
+                    "post-title"    => 
+                    "post-date"     => 
+                    "post-author"   => 
+                    "post-content"  =>
+                    
+        )
+        
+            );
+
+?>
---
+++ includes/sidebar.php
@@ -0,0 +1,31 @@
+<div class="col-sm-3 col-sm-offset-1 blog-sidebar">
+          <div class="sidebar-module sidebar-module-inset">
+            <h4>About</h4>
+            <p>I feel like a child <em> again -</em> Unlearning what I thought I knew and learning things I didn't know before.</p>
+          </div>
+          <div class="sidebar-module">
+            <h4>Archives</h4>
+            <ol class="list-unstyled">
+              <li><a href="#">March 2015</a></li>
+              <li><a href="#">February 2015</a></li>
+              <li><a href="#">January 2015</a></li>
+              <li><a href="#">December 2015</a></li>
+              <li><a href="#">November 2015</a></li>
+              <li><a href="#">October 2015</a></li>
+              <li><a href="#">September 2015</a></li>
+              <li><a href="#">August 2015</a></li>
+              <li><a href="#">July 2015</a></li>
+              <li><a href="#">June 2015</a></li>
+              <li><a href="#">May 2015</a></li>
+              <li><a href="#">April 2015</a></li>
+            </ol>
+          </div>
+          <div class="sidebar-module">
+            <h4>Elsewhere</h4>
+            <ol class="list-unstyled">
+              <li><a href="#">GitHub</a></li>
+              <li><a href="#">Twitter</a></li>
+              <li><a href="#">Facebook</a></li>
+            </ol>
+          </div>
+        </div><!-- /.blog-sidebar -->
